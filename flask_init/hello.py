2a17db795f94f021a5af8512163b5331d3213960
vonkarmen <kevin.r.vandyk@gmail.com>
2018-08-11 13:39:39 -0400

    forms

--- flask_init/hello.py
+++ flask_init/hello.py
@@ -1,15 +1,28 @@
 # import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
 
-@app.route('/hello')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'], request.form['password']):
+            return f'Welcome back {request.form["username"]}'
+        else:
+            error = 'Incorrect Username and Password'
+
+    return render_template('login.html', error=error)
+
+
+def valid_login(username, password):
+    if username == password:
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
