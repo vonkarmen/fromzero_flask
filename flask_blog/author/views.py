acc9bd4343901f53d84aa7cb44d03c47a5a79d82
vonkarmen <kevin.r.vandyk@gmail.com>
2018-08-16 23:38:03 -0400

    login and login required

--- flask_blog/author/views.py
+++ flask_blog/author/views.py
@@ -1,11 +1,34 @@
 from flask_blog import app
-from flask import render_template, redirect, url_for
-from author.form import RegisterForm
+from flask import render_template, redirect, url_for, session, request
+from author.form import RegisterForm, LoginForm
+from author.models import Author
+from author.decorators import login_required
 
 
-@app.route('/login')
+@app.route('/login', methods=('GET', 'POST'))
 def login():
-    return "Hello, User!"
+    form = LoginForm()
+    error = None
+
+    if request.method == 'GET' and request.args.get('next'):
+        session['next'] = request.args.get('next', None)
+
+    if form.validate_on_submit():
+        author = Author.query.filter_by(
+            username=form.username.data,
+            password=form.password.data
+        ).limit(1)
+
+        if author.count():
+            session['username'] = form.username.data
+            if 'next' in session:
+                next_url = session.get('next')
+                session.pop('next')
+                return redirect(next_url)
+            else:
+                return redirect(url_for('login_success'))
+
+    return render_template('author/login.html', form=form, error=error)
 
 
 @app.route('/register', methods=('GET', 'POST'))
@@ -19,3 +42,9 @@ def register():
 @app.route('/success')
 def success():
     return '<h2>Author Registered!</h2>'
+
+
+@app.route('/login/success')
+@login_required
+def login_success():
+    return '<h2>You are logged in!</h2>'
--- flask_blog/blog/views.py
+++ flask_blog/blog/views.py
@@ -3,25 +3,58 @@ from flask import render_template, redirect, flash, url_for
 from blog.form import SetupForm
 from author.models import Author
 from blog.models import Blog
+from author.decorators import login_required
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return '<h2>Hello World</h2>'
-
-
-@app.route('/admin')
-def admin():
     blogs = Blog.query.count()
     if blogs == 0:
         return redirect(url_for('setup'))
 
+    return '<h2>Hello World</h2>'
+
+
+@app.route('/admin')
+@login_required
+def admin():
     return render_template('blog/admin.html')
 
 
-@app.route('/setup')
+@app.route('/setup', methods=['POST', 'GET'])
 def setup():
     form = SetupForm()
+    error = ''
+    if form.validate_on_submit():
+        author = Author(
+            form.fullname.data,
+            form.email.data,
+            form.username.data,
+            form.password.data,
+            True
+        )
+        db.session.add(author)
+        db.session.flush()
+        if author.id:
+            blog = Blog(
+                form.name.data,
+                author.id
+            )
+            db.session.add(blog)
+            db.session.flush()
+        else:
+            db.session.rollback()
+            error = "Error creating user"
+            print(error)
 
-    return render_template('blog/setup.html', form=form)
+        if author.id and blog.id:
+            db.session.commit()
+            flash('Blog created')
+            return redirect(url_for('admin'))
+        else:
+            db.session.rollback()
+            error = "Error creating blog"
+            print(error)
+
+    return render_template('blog/setup.html', form=form, error=error)
